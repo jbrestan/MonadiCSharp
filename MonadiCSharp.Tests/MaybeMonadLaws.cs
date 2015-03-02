46e12fc34d5e8d2e695a31c54b32134ce1eec4f2
Honza Brestan <jan.brestan@live.com>
2015-03-03 00:33:45 +0100

    Added Maybe monad laws properties

---
+++ MonadiCSharp.Tests/MaybeMonadLaws.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using FsCheck;
+using FsCheck.Xunit;
+
+namespace MonadiCSharp.Tests
+{
+    public class MaybeMonadLaws
+    {
+        [Property(Arbitrary = new [] {typeof(MaybeArbitrary)})]
+        public bool LeftIdentity(NonNull<object> o, Func<object, IMaybe<object>> f)
+        {
+            return o.Item.ToMaybe().Bind(f).Equals(f(o.Item));
+        }
+
+        [Property(Arbitrary = new[] { typeof(MaybeArbitrary) })]
+        public bool RightIdentity(IMaybe<object> m)
+        {
+            return m.Bind(MaybeExtensions.ToMaybe).Equals(m);
+        }
+
+        [Property(Arbitrary = new[] { typeof(MaybeArbitrary) })]
+        public bool Associativity(NonNull<IMaybe<object>> m, Func<object, IMaybe<object>> f, Func<object, IMaybe<object>> g)
+        {
+            return m.Item.Bind(f).Bind(g).Equals(m.Item.Bind(v => f(v).Bind(g)));
+        }
+    }
+}
