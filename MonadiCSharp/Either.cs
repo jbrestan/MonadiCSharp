52860aca5875e14171881cff93507faab17f49c8
Honza Brestan <jan.brestan@live.com>
2015-08-04 01:14:22 +0200

    Formatting

--- MonadiCSharp/Either.cs
+++ MonadiCSharp/Either.cs
@@ -5,6 +5,7 @@ namespace MonadiCSharp
     public static class Either
     {
         public static IEither<TLeft, TRight> Left<TLeft, TRight>(TLeft value) => new Left<TLeft, TRight>(value);
+
         public static IEither<TLeft, TRight> Right<TLeft, TRight>(TRight value) => new Right<TLeft, TRight>(value);
     }
 }
